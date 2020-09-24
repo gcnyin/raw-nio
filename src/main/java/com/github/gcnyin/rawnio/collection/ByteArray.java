e22be7fdb0cd019fd94e1cd70fc019f75c4e36f3
gcnyin <guchounongyin@gmail.com>
2020-09-24 17:18:16 +0800

    add ByteArray::getInt

--- src/main/java/com/github/gcnyin/rawnio/collection/ByteArray.java
+++ src/main/java/com/github/gcnyin/rawnio/collection/ByteArray.java
@@ -1,24 +1,38 @@
-package com.github.gcnyin.rawnio.utils;
+package com.github.gcnyin.rawnio.collection;
+
+import lombok.Getter;
 
 import java.util.Arrays;
 
 public class ByteArray {
   private byte[] source = {};
+  @Getter
+  private long size = 0;
 
   public ByteArray add(byte... bytes) {
     int sourceOriginLength = source.length;
     int bytesLength = bytes.length;
     source = Arrays.copyOf(source, sourceOriginLength + bytesLength);
     System.arraycopy(bytes, 0, source, sourceOriginLength, bytesLength);
+    size += bytesLength;
     return this;
   }
 
-  public byte[] getCopy() {
+  public int getInt(int index) {
+    byte first = source[index];
+    byte second = source[index + 1];
+    byte third = source[index + 2];
+    byte forth = source[index + 3];
+    return first << 24 | (second & 0xFF) << 16 | (third & 0xFF) << 8 | (forth & 0xFF);
+  }
+
+  public byte[] getCopyArray() {
     return source.clone();
   }
 
   public ByteArray clear() {
     source = new byte[]{};
+    size = 0;
     return this;
   }
 }
--- src/test/java/com/github/gcnyin/rawnio/utils/ByteArrayTest.java
+++ src/test/java/com/github/gcnyin/rawnio/utils/ByteArrayTest.java
@@ -1,16 +1,45 @@
 package com.github.gcnyin.rawnio.utils;
 
+import com.github.gcnyin.rawnio.collection.ByteArray;
 import org.junit.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 public class ByteArrayTest {
   @Test
-  public void should_return_inserted_data_when_insert_some_bytes_and_call_getCopy_given_a_ByteArrry() {
+  public void should_return_inserted_data_when_insert_some_bytes_and_call_getCopyArray_given_a_ByteArray() {
     ByteArray byteArray = new ByteArray();
     byteArray.add(new byte[]{1, 2, 3})
       .add(new byte[]{4, 5, 6});
-    byte[] copy = byteArray.getCopy();
+    byte[] copy = byteArray.getCopyArray();
     assertArrayEquals(new byte[]{1, 2, 3, 4, 5, 6}, copy);
+    assertEquals(6, byteArray.getSize());
+  }
+
+  @Test
+  public void should_clear_data_when_call_clear_given_a_ByteArray() {
+    ByteArray byteArray = new ByteArray();
+    byteArray.add(new byte[]{1, 2, 3})
+      .add(new byte[]{4, 5, 6});
+    byte[] copy = byteArray.clear().getCopyArray();
+    assertArrayEquals(new byte[]{}, copy);
+    assertEquals(0, byteArray.getSize());
+  }
+
+  @Test
+  public void should_return_int_when_call_getInt_given_a_ByteArray_contains_4_bytes() {
+    ByteArray byteArray = new ByteArray();
+    int i1 = byteArray.add(new byte[]{1, 2, 3, 4}).getInt(0);
+    assertEquals(16909060, i1);
+    int i2 = byteArray.add(new byte[]{5}).getInt(1);
+    System.out.println(i2);
+  }
+
+  @Test
+  public void should_throw_ArrayIndexOutOfBoundsException_when_call_getInt_with_param_1_given_a_ByteArray_only_contains_4_bytes() {
+    ByteArray byteArray = new ByteArray();
+    assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+      byteArray.add(new byte[]{1, 2, 3, 4}).getInt(1);
+    });
   }
 }
